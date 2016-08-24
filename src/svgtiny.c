f293a45e808b444b5a3b8b989b76fdc20566d3c9
Vincent Sanders <vince@kyllikki.org>
2016-08-24 22:02:47 +0100

    cope with lack of root svg element

--- src/svgtiny.c
+++ src/svgtiny.c
@@ -188,6 +188,12 @@ svgtiny_code svgtiny_parse(struct svgtiny_diagram *diagram,
 		dom_node_unref(document);
 		return svgtiny_LIBDOM_ERROR;
 	}
+        if (svg == NULL) {
+                /* no root svg element */
+                dom_node_unref(document);
+		return svgtiny_SVG_ERROR;
+        }
+
 	exc = dom_node_get_node_name(svg, &svg_name);
 	if (exc != DOM_NO_ERR) {
 		dom_node_unref(svg);
